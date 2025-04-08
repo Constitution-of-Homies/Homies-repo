76b05959571ef247a3c3c8cebdfb49751da29c11
2653934 <2653934@students.wits.ac.za>
2025-04-08 23:27:38 +0200

    clean up project directory and add mock html landing page

---
+++ client/js/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDyLL5lej7NYTIi9udmCwe_l7HsVb7e-AQ",
+  authDomain: "constitution-of-homies.firebaseapp.com",
+  projectId: "constitution-of-homies",
+  storageBucket: "constitution-of-homies.firebasestorage.app",
+  messagingSenderId: "534709453915",
+  appId: "1:534709453915:web:855e13de6ef93393f16b7e",
+  measurementId: "G-9SCYLHCB0L"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = firebase.auth();
